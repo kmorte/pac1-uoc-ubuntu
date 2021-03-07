1dbe2cb063405315ea9a1a9e27d1595edd3e595a
kmorte <kmorte@LAPTOP-P13MGNHE.localdomain>
2021-03-07 16:40:22 +0100

    building site

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@
 console.log('Food site')
 console.log('For the uoc Master')
 console.log('Welcome');
+
+
+const scrollTo = (target, duration) => {
+    const targetEl = document.querySelector(target);
+    console.log(targetEl);
+}
+
+scrollTo('#hamburger')
+const a = JSON.stringify({kev: 'kevin'})
+const b = JSON.parse(a)
+
+console.log(a);
+console.log(b);
