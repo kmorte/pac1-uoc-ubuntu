5065bd9ddfdbf89a35e2f4fa3f7ed75b23d530ed
kmorte <kmorte@LAPTOP-P13MGNHE.localdomain>
2021-03-30 01:13:37 +0200

    build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const textBtn = document.querySelector('#opening')
 console.log(textBtn)
 const elevator = new Elevator({
     element: textBtn,
-    mainAudio: './src/music/music.mp3', // Music from http://www.bensound.com/
-    endAudio:  './src/music/ding.mp3'
+    mainAudio: './music/music.mp3', // Music from http://www.bensound.com/
+    endAudio:  './music/ding.mp3'
 });
