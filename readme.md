8219899e6b80ac078fc1ae61f9ea2ffa2fc9c2c5
kmorte <kmorte@LAPTOP-P13MGNHE.localdomain>
2021-04-05 02:45:55 +0200

    builsing site.

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 
 
+Food & Fiction
+La tasca és construir un espai web relacionat amb la gastronomia. Food & Fiction es un web fictici que rememora alguns conceptes o elements gastronòmics reals només en algunes ficcions (televisives). D'aquesta manera, podem trobar l'hamburguesa de cranc típica de Bob Esponja o el pestís de cireres tan llorejat per un dels personatges a la sèrie de televisió Twin Peaks. Cal mencionar que la part "Fiction" de Food & Fiction no es estricte, perqué alguns elements són certament representacions del món real, com el cas de l'establiment Big Boy's o The cheesecake Factory. De vegades ficció i realitat es retro representen.
 
 
 
--- src/index.js
+++ src/index.js
@@ -8,3 +8,21 @@ const elevator = new Elevator({
     mainAudio: 'https://github.com/tholman/elevator.js/blob/master/demo/music/ding.mp3', // Music from http://www.bensound.com/
     endAudio:  './music/ding.mp3'
 });
+
+//APPLY VISIBILITY HIDDEN
+
+// const menuBtn = document.querySelector('li a');
+// console.log(menuBtn);
+// window.addEventListener('click', (e) => {
+//     // e.preventDefault();
+//     if(e.target.getAttribute('href')) {
+
+//         if(document.querySelector('.inside')) {
+
+//             // console.log(document.querySelector('.inside'))
+//             document.querySelector('.inside').classList.remove('inside')
+//         }
+//         e.target.parentElement.classList.add('inside')
+
+//     }
+// })
