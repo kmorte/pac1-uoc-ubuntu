957704fa64b857e74b6e27dcc56429c3ae2cdd1f
kmorte <kmorte@LAPTOP-P13MGNHE.localdomain>
2021-04-10 07:32:38 +0200

    music works. finally

--- src/index.js
+++ src/index.js
@@ -2,27 +2,19 @@
 import Elevator from 'elevator.js'
 
 const textBtn = document.querySelector('#opening')
-console.log(textBtn)
+const music = document.querySelector('#audio')
+const ding = document.querySelector('#audio2')
+
 const elevator = new Elevator({
     element: textBtn,
-    mainAudio: 'https://github.com/tholman/elevator.js/blob/master/demo/music/ding.mp3', // Music from http://www.bensound.com/
-    endAudio:  './music/ding.mp3'
+    duration: 10000
 });
 
-//APPLY VISIBILITY HIDDEN
+textBtn.addEventListener('click', () => {
+    music.play();
+    setTimeout(() => {
+        music.pause();
+        ding.play();
 
-// const menuBtn = document.querySelector('li a');
-// console.log(menuBtn);
-// window.addEventListener('click', (e) => {
-//     // e.preventDefault();
-//     if(e.target.getAttribute('href')) {
-
-//         if(document.querySelector('.inside')) {
-
-//             // console.log(document.querySelector('.inside'))
-//             document.querySelector('.inside').classList.remove('inside')
-//         }
-//         e.target.parentElement.classList.add('inside')
-
-//     }
-// })
+    },10000)
+})
